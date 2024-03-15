ea78d32d1896f03b834a3a58c938f619b4998293
Ravi <raikantror@gmail.com>
2024-03-15 18:09:50 +0530

    Test case to handle any delimiter

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe StringCalculator do
     it "handle the sum of numbers separated by new lines and commas" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "handle the sum of numbers separated by any delimiter" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
